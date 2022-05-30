f11e047aa1065c0596ca3a9e987699dec97ecec0
Aidana <a.dyusebaeva@mail.ru>
2022-05-30 17:13:24 +0500

    Initial commit

---
+++ src/DZ2/Main.java
@@ -0,0 +1,64 @@
+package DZ2;
+
+public class Main {
+    public static void main(String[] args) {
+       int a = 7;
+       int b = 5;
+       int sum = a + b;
+
+       if(sum <= 20 && sum > 10) {
+           System.out.println("true");
+       }
+       else {
+           System.out.println("false");
+       }
+
+       int i = -15;
+       if(i < 0) {
+           System.out.println("negative");
+       }
+       else {
+           System.out.println("positive");
+       }
+
+       int k = 50;
+       if (k >= 0) {
+           System.out.println("false");
+       }
+       else {
+           System.out.println("true");
+       }
+
+
+
+       int e = 7;
+
+       public static boolean isNegative(int e) {
+           if (int >= 0) {
+                return "false";
+            }
+            else {
+                return "true";
+            }
+        }
+
+       public static void printWordNTimes(String "Voyage", int q) {
+            for (int q = 0; q < 5; q++) {
+                System.out.println("Voyage");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
